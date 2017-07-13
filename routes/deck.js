5510ccf11e4e2fe0056abd8ecda998aebfbbc45f
DavidTurkGT <dlturk747@gmail.com>
2017-07-13 16:08:40 -0400

    Decks pass all tests

--- routes/deck.js
+++ routes/deck.js
@@ -1,8 +1,135 @@
 const express = require('express');
 const router = express.Router();
+const Models = require('../models');
 
-router.get('/', (req, res) => {
-  res.send("Deck router!");
+function validateRequest (req, res, next) {
+  req.checkBody("name", "Name cannot be blank").notEmpty();
+
+  let errors = req.validationErrors();
+
+  if(errors){
+    let errMsg = [];
+    errors.forEach( (err) => errMsg.push(err.msg) );
+    res.status(400).json({errors: errMsg});
+  }
+  else{
+    //Make sure name is unique
+    Models.Deck.find({ where: {name: req.body.name} })
+    .catch( (err) => res.status(500).send(err) )
+    .then( (deck) => {
+      if(deck){
+        res.status(400).send("Deck name is already taken");
+      }
+      else{
+        next();
+      }
+    })
+  }
+}
+
+function findDeck (req, res, next) {
+  Models.Deck.find({ where: {id: req.params.deckId} })
+  .catch( (err) => res.status(500).send(err) )
+  .then( (deck) => {
+    if(!deck){
+      res.status(404).send("No deck found");
+    }
+    else{
+      //Retrieve User
+      Models.User.findById(deck.userID)
+      .catch( (err) => res.status(500).send(err) )
+      .then( (user) => {
+        if(!user){
+          res.status(404).send("No associated user");
+        }
+        else{
+          deck.user = user.dataValues;
+          //Find associated cards
+          Models.Flipcard.findAll({
+            where: {deckID: req.params.deckId}
+          })
+          .catch( (err) => res.status(500).send(err) )
+          .then( (cards) => {
+            let foundCards = [];
+            cards.forEach( (card) => foundCards.push(card.dataValues) );
+            deck.cards = foundCards;
+            req.deck = deck;
+            next();
+          })
+        }
+      })
+    }
+  })
+}
+
+function deleteCards (req, res, next) {
+  Models.Flipcard.findAll({ where: { deckID: req.params.deckId } })
+  .catch( (err) => res.status(500).send(err) )
+  .then( (cards) => {
+    cards.forEach( (card) => {
+      card.destroy().catch( (err) => res.status(500).send(err) );
+    });
+    next();
+  });
+}
+
+router.post('/for/:userId', validateRequest, (req, res) => {
+  //Make sure the user is a valid user
+  Models.User.find({ where: {id: req.params.userId} })
+  .catch( (err) => res.status(500).send(err) )
+  .then( (user) => {
+    if(!user){
+      res.status(404).send("Not a valid user");
+    }
+    else{
+      let newDeck = {
+        name: req.body.name,
+        userID: req.params.userId
+      };
+      Models.Deck.create(newDeck)
+      .catch( (err) => res.status(500).send(err) )
+      .then( (newDeck) => {
+        res.status(200).json({deck: newDeck});
+      })
+    }
+  })
+});
+
+router.get('/:deckId', findDeck, (req, res) => {
+  let deck = req.deck.dataValues;
+  deck.user = req.deck.user;
+  deck.cards = req.deck.cards;
+  res.status(200).json({deck: deck});
+});
+
+router.put('/:deckId', (req, res) => {
+  Models.Deck.findById(req.params.deckId)
+  .catch( (err) => res.status(500).send(err) )
+  .then( (deck) => {
+    if(!deck){
+      res.status(404).send('No deck found');
+    }
+    else{
+      deck.name = req.body.name || deck.name;
+      deck.save()
+      .catch( (err) => res.status(500).send(err) )
+      .then( (deck) => {
+        res.status(200).json({deck: deck});
+      });
+    }
+  });
+});
+
+router.delete('/:deckId', findDeck, deleteCards, (req, res) => {
+  Models.Deck.findById(req.params.deckId)
+  .catch( (err) => res.status(500).send(err) )
+  .then( (deck) => {
+    deck.destroy()
+    .catch( (err) => res.status(500).send(err) )
+    .then( () => {
+      res.status(200).json({deck: deck});
+    });
+  });
 });
 
 module.exports = router;
--- test/decks.js
+++ test/decks.js
@@ -1,237 +1,240 @@
-// const assert = require('assert');
-// const request = require('supertest');
-// const Models = require('../models');
-// const app = require('../server');
-//
-// let User, Deck, Card;
-//
-// before("Setting up...", (done) => {
-//   let newUser = {
-//     username: 'testUser',
-//     passwordSalt: 'testSalt',
-//     passwordIterations: 2000,
-//     passwordHash: 'testHash'
-//   };
-//   Models.User.create(newUser).then( (newUser) => {
-//     User = newUser;
-//     let newDeck = {
-//       name: "testDeck",
-//       userID: newUser.dataValues.id
-//     };
-//     Models.Deck.create(newDeck).then( (newDeck) => {
-//       Deck = newDeck;
-//       let newCard = {
-//         question: "Are your tests passing?",
-//         answer: "No",
-//         deckID: newDeck.dataValues.id
-//       };
-//       Models.Flipcard.create(newCard).then( (newCard) => {
-//         Card = newCard;
-//         done();
-//       });
-//     });
-//
-//   });
-// });
-//
-// after("Cleaning up...", (done) => {
-//   Models.Flipcard.destroy({
-//     where: {}
-//   }).then( () => {
-//     Models.Deck.destroy({
-//       where: {}
-//     }).then( () => {
-//       Models.User.destroy({
-//         where: {}
-//       }).then( () => {
-//         done();
-//       });
-//     });
-//   });
-// });
-//
-// describe("A deck", () => {
-//
-//   it("can be created" , (done) => {
-//     request(app)
-//       .post('/deck/for/'+User.id)
-//       .send({name: "newDeck"})
-//       .expect(200)
-//       .expect('Content-Type', 'application/json; charset=utf-8')
-//       .expect( (res) => {
-//         assert(res, "No response received");
-//         assert(res.body, "No body sent with the response");
-//         assert(res.body.deck, "No deck sent with the response");
-//         let newDeck = res.body.deck;
-//         assert(newDeck.name, "Deck has no name");
-//         assert.equal(newDeck.name, "newDeck", "Name of new deck is not newDeck. Received: " + newDeck.name);
-//         assert(newDeck.userID, "Deck has no association with a user");
-//         assert.equal(newDeck.userID,User.id,"Associated user ID is not equal to 1. Received: " + newDeck.userID)
-//       })
-//       .end( (err, res) => {
-//         if(err) done(err);
-//         else done();
-//       });
-//   });
-//
-//   it("will throw an error if name is blank when created", (done) => {
-//     request(app)
-//     .post('/deck/for/'+User.id)
-//     .send({name: ""})
-//     .expect(400)
-//     .end( (err, res) => {
-//       if(err) done(err)
-//       else done();
-//     });
-//   });
-//
-//   it("will throw and error if the name is already taken when created", (done) => {
-//     request(app)
-//     .post('/deck/for/'+User.id)
-//     .send({name: "testDeck"})
-//     .expect(400)
-//     .end( (err, res) => {
-//       if(err) done(err);
-//       else done();
-//     });
-//   });
-//
-//   it("will throw an error if an associated user is not found when created", (done) => {
-//     request(app)
-//     .post('/deck/for/1989')
-//     .send({name: "ThisWillFail"})
-//     .expect(404)
-//     .end( (err, res) => {
-//       if(err) done(err);
-//       else done();
-//     });
-//   });
-//
-//   it("can be retrieved from the database", (done) => {
-//     request(app)
-//       .get("/deck/"+Deck.id)
-//       .expect(200)
-//       .expect('Content-Type','application/json; charset=utf-8')
-//       .expect( (res) => {
-//         assert(res, 'No response sent');
-//         assert(res.body, 'No body attached to response');
-//         assert(res.body.deck, 'No deck sent with the response');
-//         let retrievedDeck = res.body.deck;
-//         assert(retrievedDeck.id, "Deck has no id");
-//         assert.equal(retrievedDeck.id, Deck.id, "ID's do not match. Received: " +retrievedDeck.id +". Expected: "+Deck.id);
-//         assert(retrievedDeck.name, 'A deck has no name');
-//         assert.equal(retrievedDeck.name,Deck.name, "Names do not match. Received: "+retrievedDeck.name+". Expected: "+Deck.name);
-//       })
-//       .end( (err, res) => {
-//         if(err) done(err);
-//         else done();
-//       });
-//   });
-//
-//   it("throws an error when a deck is not found", (done) => {
-//     request(app)
-//     .get('/deck/1989')
-//     .expect(404)
-//     .end( (err, res) =>{
-//       if(err) done(err);
-//       else done();
-//     });
-//   });
-//
-//   it("is associated with a user", (done) => {
-//     request(app)
-//       .get('/deck/'+Deck.id)
-//       .expect(200)
-//       .expect('Content-Type','applicaton/json; charset=utf-8')
-//       .expect( (res) => {
-//         assert(res, 'No response received');
-//         assert(res.body, 'No response body received');
-//         assert(res.body.deck, 'No deck received');
-//         let retrievedDeck = res.body.deck;
-//         assert(retrievedDeck.user, 'Deck has no user associated');
-//         let retrievedUser = retrievedDeck.user;
-//         assert(retrievedUser.id, 'Associated user has no ID');
-//         assert.equal(retrievedUser.id, User.id, "ID's do not match. Expected: "+retrievedUser.id+". Expected: "+User.id);
-//         assert(retrievedUser.username, 'Associated user has no username');
-//       })
-//       .end( (err, res) => {
-//         if(err) done(err);
-//         else done();
-//       })
-//   });
-//
-//   it("has cards associated with it", (done) => {
-//     request(app)
-//       .get('/deck/'+Deck.id)
-//       .expect(200)
-//       .expect('Content-Type','applicaton/json; charset=utf-8')
-//       .expect( (res) => {
-//         assert(res, 'No response received');
-//         assert(res.body, 'No response body received');
-//         assert(res.body.deck, 'No deck received');
-//         let retrievedDeck = res.body.deck;
-//         assert(retrievedDeck.cards, 'No cards associated with deck');
-//         assert.equal(retrievedDeck.cards[0].id, Card.id, "Card ID's do not match. Received: "+retrievedDeck.cards[0].id+". Expected: "+Card.id);
-//       })
-//       .end( (err, res) => {
-//         if(err) done(err);
-//         else done(err)
-//       })
-//   });
-//
-//   it("can be updated with a new name", (done) => {
-//     request(app)
-//       .put('/deck/'+Deck.id)
-//       .send({ name: "totallyNewDeck" })
-//       .expect(200)
-//       .expect('Content-Type','applicaton/json; charset=utf-8')
-//       .expect( (res) => {
-//         assert(res, 'No response received');
-//         assert(res.body, 'No response body received');
-//         assert(res.body.deck, 'No deck received');
-//         let retrievedDeck = res.body.deck;
-//         assert.equal(retrievedDeck.name,"totallyNewDeck","Names do not match. Received: "+retrievedDeck.name+". Expected: totallyNewDeck");
-//       })
-//       .end( (err, res) => {if(err) done(err) });
-//   });
-//
-//   it("will not be updated if no deck is found; instead throws and error", (done) => {
-//     request(app)
-//       .put('/deck/1989')
-//       .expect(404)
-//       .end( (err, res) => {
-//         if(err) done(err);
-//         else done();
-//       });
-//   })
-//
-//   it("can be deleted from the database", (done) => {
-//     //Test deleting a deck
-//     request(app)
-//       .delete('/deck/'+Deck.id)
-//       .expect(200)
-//       .end( (err, res) => {
-//         if(err) done(err);
-//         else{
-//           request(app)
-//           .get('/deck/'+Deck.id)
-//           .expect(404)
-//           .end( (err, res) => {
-//             if(err) done(err);
-//             else done();
-//           });
-//         }
-//       })
-//   });
-//
-//   it("will throw an error if no deck is found when deleting", (done) => {
-//     request(app)
-//       .delete('/deck/'+Deck.id)
-//       .expect(404)
-//       .end( (err, res) => {
-//         if(err) done(err);
-//         else done();
-//       });
-//   });
-//
-// });
+const assert = require('assert');
+const request = require('supertest');
+const Models = require('../models');
+const app = require('../server');
+
+let User, Deck, Card;
+
+before("Setting up...", (done) => {
+  let newUser = {
+    username: 'testUser',
+    passwordSalt: 'testSalt',
+    passwordIterations: 2000,
+    passwordHash: 'testHash'
+  };
+  Models.User.create(newUser).then( (newUser) => {
+    User = newUser;
+    let newDeck = {
+      name: "testDeck",
+      userID: newUser.dataValues.id
+    };
+    Models.Deck.create(newDeck).then( (newDeck) => {
+      Deck = newDeck;
+      let newCard = {
+        question: "Are your tests passing?",
+        answer: "No",
+        deckID: newDeck.dataValues.id
+      };
+      Models.Flipcard.create(newCard).then( (newCard) => {
+        Card = newCard;
+        done();
+      });
+    });
+
+  });
+});
+
+after("Cleaning up...", (done) => {
+  Models.Flipcard.destroy({
+    where: {}
+  }).then( () => {
+    Models.Deck.destroy({
+      where: {}
+    }).then( () => {
+      Models.User.destroy({
+        where: {}
+      }).then( () => {
+        done();
+      });
+    });
+  });
+});
+
+describe("A deck", () => {
+
+  it("can be created" , (done) => {
+    request(app)
+      .post('/deck/for/'+User.id)
+      .send({name: "newDeck"})
+      .expect(200)
+      .expect('Content-Type', 'application/json; charset=utf-8')
+      .expect( (res) => {
+        assert(res, "No response received");
+        assert(res.body, "No body sent with the response");
+        assert(res.body.deck, "No deck sent with the response");
+        let newDeck = res.body.deck;
+        assert(newDeck.name, "Deck has no name");
+        assert.equal(newDeck.name, "newDeck", "Name of new deck is not newDeck. Received: " + newDeck.name);
+        assert(newDeck.userID, "Deck has no association with a user");
+        assert.equal(newDeck.userID,User.id,"Associated user ID is not equal to 1. Received: " + newDeck.userID)
+      })
+      .end( (err, res) => {
+        if(err) done(err);
+        else done();
+      });
+  });
+
+  it("will throw an error if name is blank when created", (done) => {
+    request(app)
+    .post('/deck/for/'+User.id)
+    .send({name: ""})
+    .expect(400)
+    .end( (err, res) => {
+      if(err) done(err)
+      else done();
+    });
+  });
+
+  it("will throw and error if the name is already taken when created", (done) => {
+    request(app)
+    .post('/deck/for/'+User.id)
+    .send({name: "testDeck"})
+    .expect(400)
+    .end( (err, res) => {
+      if(err) done(err);
+      else done();
+    });
+  });
+
+  it("will throw an error if an associated user is not found when created", (done) => {
+    request(app)
+    .post('/deck/for/1989')
+    .send({name: "ThisWillFail"})
+    .expect(404)
+    .end( (err, res) => {
+      if(err) done(err);
+      else done();
+    });
+  });
+
+  it("can be retrieved from the database", (done) => {
+    request(app)
+      .get("/deck/"+Deck.id)
+      .expect(200)
+      .expect('Content-Type','application/json; charset=utf-8')
+      .expect( (res) => {
+        assert(res, 'No response sent');
+        assert(res.body, 'No body attached to response');
+        assert(res.body.deck, 'No deck sent with the response');
+        let retrievedDeck = res.body.deck;
+        assert(retrievedDeck.id, "Deck has no id");
+        assert.equal(retrievedDeck.id, Deck.id, "ID's do not match. Received: " +retrievedDeck.id +". Expected: "+Deck.id);
+        assert(retrievedDeck.name, 'A deck has no name');
+        assert.equal(retrievedDeck.name,Deck.name, "Names do not match. Received: "+retrievedDeck.name+". Expected: "+Deck.name);
+      })
+      .end( (err, res) => {
+        if(err) done(err);
+        else done();
+      });
+  });
+
+  it("throws an error when a deck is not found", (done) => {
+    request(app)
+    .get('/deck/1989')
+    .expect(404)
+    .end( (err, res) =>{
+      if(err) done(err);
+      else done();
+    });
+  });
+
+  it("is associated with a user", (done) => {
+    request(app)
+      .get('/deck/'+Deck.id)
+      .expect(200)
+      .expect('Content-Type','application/json; charset=utf-8')
+      .expect( (res) => {
+        assert(res, 'No response received');
+        assert(res.body, 'No response body received');
+        assert(res.body.deck, 'No deck received');
+        let retrievedDeck = res.body.deck;
+        assert(retrievedDeck.user, 'Deck has no user associated');
+        let retrievedUser = retrievedDeck.user;
+        assert(retrievedUser.id, 'Associated user has no ID');
+        assert.equal(retrievedUser.id, User.id, "ID's do not match. Expected: "+retrievedUser.id+". Expected: "+User.id);
+        assert(retrievedUser.username, 'Associated user has no username');
+      })
+      .end( (err, res) => {
+        if(err) done(err);
+        else done();
+      })
+  });
+
+  it("has cards associated with it", (done) => {
+    request(app)
+      .get('/deck/'+Deck.id)
+      .expect(200)
+      .expect('Content-Type','application/json; charset=utf-8')
+      .expect( (res) => {
+        assert(res, 'No response received');
+        assert(res.body, 'No response body received');
+        assert(res.body.deck, 'No deck received');
+        let retrievedDeck = res.body.deck;
+        assert(retrievedDeck.cards, 'No cards associated with deck');
+        assert.equal(retrievedDeck.cards[0].id, Card.id, "Card ID's do not match. Received: "+retrievedDeck.cards[0].id+". Expected: "+Card.id);
+      })
+      .end( (err, res) => {
+        if(err) done(err);
+        else done(err)
+      })
+  });
+
+  it("can be updated with a new name", (done) => {
+    request(app)
+      .put('/deck/'+Deck.id)
+      .send({ name: "totallyNewDeck" })
+      .expect(200)
+      .expect('Content-Type','application/json; charset=utf-8')
+      .expect( (res) => {
+        assert(res, 'No response received');
+        assert(res.body, 'No response body received');
+        assert(res.body.deck, 'No deck received');
+        let retrievedDeck = res.body.deck;
+        assert.equal(retrievedDeck.name,"totallyNewDeck","Names do not match. Received: "+retrievedDeck.name+". Expected: totallyNewDeck");
+      })
+      .end( (err, res) => {
+        if(err) done(err);
+        else done();
+      });
+  });
+
+  it("will not be updated if no deck is found; instead throws and error", (done) => {
+    request(app)
+      .put('/deck/1989')
+      .expect(404)
+      .end( (err, res) => {
+        if(err) done(err);
+        else done();
+      });
+  });
+
+  it("can be deleted from the database", (done) => {
+    //Test deleting a deck
+    request(app)
+      .delete('/deck/'+Deck.id)
+      .expect(200)
+      .end( (err, res) => {
+        if(err) done(err);
+        else{
+          request(app)
+          .get('/deck/'+Deck.id)
+          .expect(404)
+          .end( (err, res) => {
+            if(err) done(err);
+            else done();
+          });
+        }
+      })
+  });
+
+  it("will throw an error if no deck is found when deleting", (done) => {
+    request(app)
+      .delete('/deck/'+Deck.id)
+      .expect(404)
+      .end( (err, res) => {
+        if(err) done(err);
+        else done();
+      });
+  });
+
+});
